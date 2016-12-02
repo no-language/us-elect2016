d5411b0a5380b4790979c6304497dcb2a16014dd
Dylan Moeller <dylanmoeller@gmail.com>
2016-12-02 01:49:35 -0500

    complete candidate information parsing script

--- src/clean_candidates.R
+++ src/clean_candidates.R
@@ -8,10 +8,10 @@ library(stringr)
 files <- paste0('./data/electoral-returns/raw/', seq(1972, 2012, 4), '.txt')
 raw <- lapply(files, scan, what = 'character', sep = '\n')
 
-parse_raw_candidates <- function(raw_year) {
+parse_raw_candidates <- function(raw_year, year, start_buffer = 2) {
   # Prior to 2000, candidate names begin 2 lines below "POPULAR VOTE AND  
   # ELECTORAL COLLEGE VOTE BY STATE" and continue for three lines.
-  start <- grep("POPULAR VOTE AND ELECTORAL COLLEGE", raw_year) + 2
+  start <- grep("POPULAR VOTE AND ELECTORAL COLLEGE", raw_year) + start_buffer
   raw_candidates <- raw_year[seq(start, start + 2)]
   
   raw_candidates <- raw_candidates %>%
@@ -28,14 +28,15 @@ parse_raw_candidates <- function(raw_year) {
     `[`( , 2)
   
   data.frame(
+    year = year,
     first_name = raw_candidates[[1]],
     last_name = raw_candidates[[2]],
     party = party,
-    state = state
+    home_state = state
   )
 }
 
-parse_candidate_summary <- function(raw_year) {
+parse_candidate_summary <- function(raw_year, year) {
   # Parse candidate information from text files that contain a summary of the
   # performance of each candidate in a given election year.
   
@@ -45,8 +46,13 @@ parse_candidate_summary <- function(raw_year) {
   end <- grep('^-+', raw_year[start:length(raw_year)])[1] + start - 2
   
   raw_year <- raw_year[start:end] %>%
-    str_split('\\s{2,}') %>%
-    lapply(`[`, 1:3)
+    str_split('\\s{2,}')
+  # Correct any entries with no party affiliation.
+  raw_year <- lapply(raw_year, function(x) {
+    if (length(x) == 6) return(x)
+    else return(c(x[1], NA, x[2:length(x)]))
+  })
+  raw_year <- lapply(raw_year, `[`, 1:3)
   raw_year <- do.call(rbind, raw_year[seq(1, length(raw_year) - 1)])
   
   candidate_name <- str_split(raw_year[, 1], '\\s{1}')
@@ -56,10 +62,11 @@ parse_candidate_summary <- function(raw_year) {
   last_name <- lapply(candidate_name, function(i) i[length(i)])
   
   data.frame(
+    year = year,
     first_name = unlist(first_name),
     last_name = unlist(last_name),
     party = raw_year[, 2],
-    state = raw_year[, 3]
+    home_state = raw_year[, 3]
   )
 }
 
@@ -67,10 +74,21 @@ parse_candidates <- function(year) {
   if (year == 2004) {
     stop('The 2004 electoral returns file does not contain the full set ',
          'of candidate information.')
+  } else if (!(year %in% seq(1972, 2012, 4))) {
+    stop(paste0('Electoral return data not available for', year))
   }
   file <- paste0('./data/electoral-returns/raw/', year, '.txt')
   raw <- scan(file, what = 'character', sep = '\n')
   
-  if (year < 2000) return(parse_raw_candidates(raw))
-  else return(parse_candidate_summary(raw))
+  if (year < 2000) return(parse_raw_candidates(raw, year))
+  else if (year == 2000) return (parse_raw_candidates(raw, year, 5))
+  else return(parse_candidate_summary(raw, year))
 }
+
+years <- c(seq(1972, 2000, 4), 2008, 2012)
+candidates <- years %>%
+  lapply(parse_candidates) %>%
+  bind_rows()
+
+write.csv(candidates, './data/electoral-returns/clean/candidates.csv',
+          row.names = FALSE)
